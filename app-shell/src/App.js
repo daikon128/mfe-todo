88fb3c7df74581ad74ec8c9a4c4dbf8ea975d50e
daikon128 <ndaichi06@gmail.com>
2021-12-20 18:08:06 +0900

    initial commit

---
+++ app-shell/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// const Button = React.lazy(() => import("todo-list/Button"));
+import Button from "todolist/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
