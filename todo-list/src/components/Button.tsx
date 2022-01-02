d82ede25c9108c03bcfc8c80602d792ea1ff3f22
daikon128 <daikon128@example.com>
2022-01-02 23:32:36 +0900

    buttonにhtmlのeventをお試しコードを追加

--- todo-list/src/components/Button.tsx
+++ todo-list/src/components/Button.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Button = () => {
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.dir(e)
+  }
+
   return (
     <>
       <div>this is TodoList&#39;s button</div>
-      <button>ts todo button</button>
+      <button onClick={handleOnClick}>ts todo button</button>
     </>
   );
 };
