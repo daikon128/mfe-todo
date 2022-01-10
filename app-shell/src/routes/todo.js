864020af31fc46b09d15f7add4780e143cd0c954
daikon128 <daikon128@example.com>
2022-01-10 16:33:31 +0900

    todoTitleクリック時の処理にダミー処理を追加

--- app-shell/src/routes/todo.js
+++ app-shell/src/routes/todo.js
@@ -2,8 +2,9 @@ import React from "react";
 import TodoList from 'todo_list/TodoList'
 
 export default function Todo() {
+  const onClickTodoTitle = (id) => alert(id)
   return (
-    <TodoList></TodoList>
+    <TodoList onClickTodoTitle={onClickTodoTitle}/>
   )
 
 }
